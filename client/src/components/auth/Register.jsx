03e37daaa18a05f013cd3f84de167017cb44141c
Richard Horsford <richard.horsford@digital.hmrc.gov.uk>
2019-08-02 17:11:45 +0100

    temp commit

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,5 +1,9 @@
 import React, {Component} from "react";
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+import classnames from "classnames";
 import "./auth.scss";
 
 class Register extends Component {
@@ -14,18 +18,35 @@ class Register extends Component {
     };
   }
 
+  componentDidMount() {
+    // If logged in and user navigates to Register page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
   onChange = e => {
     this.setState({[e.target.id]: e.target.value});
   };
+
   onSubmit = e => {
     e.preventDefault();
+
     const newUser = {
       name: this.state.name,
       email: this.state.email,
       password: this.state.password,
       password2: this.state.password2
     };
-    console.log(newUser);
+    this.props.registerUser(newUser, this.props.history);
   };
 
   render() {
@@ -48,47 +69,65 @@ class Register extends Component {
                   </p>
                 </div>
                 <form noValidate onSubmit={this.onSubmit}>
-                  <div className="input-field col s12">
+                  <div className="input-field">
+                    <label htmlFor="name">Name</label>
+                    <span className="red-text">{errors.name}</span>
                     <input
                         onChange={this.onChange}
                         value={this.state.name}
                         error={errors.name}
                         id="name"
                         type="text"
+                        className={classnames("", {
+                          invalid: errors.name
+                        })}
                     />
-                    <label htmlFor="name">Name</label>
                   </div>
-                  <div className="input-field col s12">
+
+                  <div className="input-field">
+                    <label htmlFor="email">Email</label>
+                    <span className="red-text">{errors.email}</span>
                     <input
                         onChange={this.onChange}
                         value={this.state.email}
                         error={errors.email}
                         id="email"
                         type="email"
+                        className={classnames("", {
+                          invalid: errors.password
+                        })}
                     />
-                    <label htmlFor="email">Email</label>
                   </div>
-                  <div className="input-field col s12">
+                  <div className="input-field">
+                    <label htmlFor="password">Password</label>
+                    <span className="red-text">{errors.password}</span>
                     <input
                         onChange={this.onChange}
                         value={this.state.password}
                         error={errors.password}
                         id="password"
                         type="password"
+                        className={classnames("", {
+                          invalid: errors.password
+                        })}
                     />
-                    <label htmlFor="password">Password</label>
                   </div>
-                  <div className="input-field col s12">
+                  <div className="input-field">
+                    <label htmlFor="password2">Confirm Password</label>
+                    <span className="red-text">{errors.password2}</span>
                     <input
                         onChange={this.onChange}
                         value={this.state.password2}
                         error={errors.password2}
                         id="password2"
                         type="password"
+                        className={classnames("", {
+                          invalid: errors.password2
+                        })}
                     />
-                    <label htmlFor="password2">Confirm Password</label>
+
                   </div>
-                  <div className="col s12" style={{paddingLeft: "11.250px"}}>
+                  <div className="input-field">
                     <button
                         type="submit"
                         className="btn btn-primary"
@@ -103,7 +142,20 @@ class Register extends Component {
         </div>
     );
   }
-
 }
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+export default connect(
+    mapStateToProps,
+    { registerUser }
+)(withRouter(Register));
+
+// export default Register;
